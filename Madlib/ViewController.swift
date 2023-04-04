43521d88f162c7ef61371a316abd6edbef9d82c7
jdormannn <89854560+jdormannn@users.noreply.github.com>
2023-04-03 20:40:04 -0400

    Done

--- Madlib/ViewController.swift
+++ Madlib/ViewController.swift
@@ -9,6 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Name: UITextField!
+    
+    @IBOutlet weak var Brand: UITextField!
+    
+    @IBOutlet weak var Size: UITextField!
+    
+    @IBOutlet weak var Day: UITextField!
+    
+    @IBOutlet weak var Style: UITextField!
+    
+    @IBOutlet weak var Label: UILabel!
+    
+    @IBAction func Button(_ sender: UIButton) {
+        Label.text = Name.text! + " went to buy " + Brand.text! + " and he needed a Size " + Size.text! + " on " + Day.text! + " and he wanted a " + Style.text! + "."
+        
+    }
+   
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
