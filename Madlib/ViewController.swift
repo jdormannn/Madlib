cf6004f4582923299e8606691a61832e46ec6a6a
jdormannn <89854560+jdormannn@users.noreply.github.com>
2023-04-03 20:22:12 -0400

    Initial Commit

---
+++ Madlib/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Madlib
+//
+//  Created by Jordan Dorman on 4/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
